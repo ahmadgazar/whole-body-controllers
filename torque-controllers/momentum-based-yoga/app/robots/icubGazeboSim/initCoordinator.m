7ae0d6d7877c95d94ef6899f5b53ea1fb9dae20d
ahmadgazar <gazar.1692741@studenti.uniroma1.it>
2018-03-26 10:06:29 +0200

    Global contact force paramterization for momentum-based yoga

--- torque-controllers/momentum-based-yoga/app/robots/icubGazeboSim/initCoordinator.m
+++ torque-controllers/momentum-based-yoga/app/robots/icubGazeboSim/initCoordinator.m
@@ -7,12 +7,8 @@
 % Feet in contact (COORDINATOR DEMO ONLY)
 Config.LEFT_RIGHT_FOOT_IN_CONTACT = [1 1];
 
-% Initial foot on ground. If false, right foot is used as default contact
-% frame (this does not means that the other foot cannot be in contact too).
-% (COORDINATOR DEMO ONLY)
-Config.LEFT_FOOT_IN_CONTACT_AT_0 = true;
-
-% If true, the robot CoM will follow a desired reference trajectory (COORDINATOR DEMO ONLY)
+% If true, the r    xCoM_ddot       = AL*f_ext_L*constraints(1) + AR*f_ext_R*constraints(2) + gravityWrench;
+%obot CoM will follow a desired reference trajectory (COORDINATOR DEMO ONLY)
 Config.DEMO_MOVEMENTS = false;
 
 % If equal to true, the desired streamed values of the center of mass 
@@ -32,8 +28,9 @@ Sat.torque = 34;
 if sum(Config.LEFT_RIGHT_FOOT_IN_CONTACT) == 2
     
     Gain.KP_COM             = diag([50 50 50]);
-    Gain.KD_COM             = 2*sqrt(Gain.KP_COM)*0;
-    Gain.KP_AngularMomentum = 10 ;
+    Gain.KD_COM             = 2*sqrt(Gain.KP_COM)*0; %don't increase this too much because the estimated xdot_com is badly estimated
+    Gain.KI_COM             = diag([20   20    20]); 
+    Gain.KP_AngularMomentum = 50 ;
     Gain.KD_AngularMomentum = 2*sqrt(Gain.KP_AngularMomentum);
 
     % Impedances acting in the null space of the desired contact forces 
@@ -49,8 +46,9 @@ end
 % PARAMETERS FOR ONE FOOT BALANCING
 if sum(Config.LEFT_RIGHT_FOOT_IN_CONTACT) == 1
     
-    Gain.KP_COM               = diag([50  100  50]);
-    Gain.KD_COM               = diag([0   0    0]);
+    Gain.KP_COM               = diag([50  100  50]); % Kp(x_dot_CoMDesired -x_dotCoM), increasing this too much is not good since x_dotCoM is computed as x_dotCoM = Jc*nu, where nu is not accurately estimated 
+    Gain.KD_COM               = diag([0   0    0]);  % Kd(x_ddot_CoMDesired - x_ddot_CoM), start with zero first
+    Gain.KI_COM               = diag([30   30    30]);  % Ki(x_CoMDesired - x_CoM)
     Gain.KP_AngularMomentum   = 1 ;
     Gain.KD_AngularMomentum   = 1 ;
 
@@ -64,7 +62,7 @@ if sum(Config.LEFT_RIGHT_FOOT_IN_CONTACT) == 1
     impRightLeg         = [30   30   30    60    10   10];   
 end
 
-Gain.impedances         = [impTorso(1,:),impArms(1,:),impArms(1,:),impLeftLeg(1,:),impRightLeg(1,:)];
+Gain.impedances         = 2*[impTorso(1,:),impArms(1,:),impArms(1,:),impLeftLeg(1,:),impRightLeg(1,:)];
 Gain.dampings           = 0*sqrt(Gain.impedances);
 
 if (size(Gain.impedances,2) ~= ROBOT_DOF)
@@ -179,8 +177,12 @@ Sm.yogaInLoop               = false;
 % So, numberOfPoints defines the number of points used to interpolate the circle 
 % in each cicle's quadrant
 numberOfPoints               = 4;  
-forceFrictionCoefficient     = 1;  
-torsionalFrictionCoefficient = 1/75;
+delta_c                      = 10  ;%1;      %friction coefficient
+forceFrictionCoefficient     = 10  ;%1;
+delta_x                      = 10;%1/75; %torsional coeificient
+delta_y                      = 10;%1/75;
+delta_z                      = 10;%1/75;
+torsionalFrictionCoefficient = 10;%1/75;
 
 % physical size of the foot                             
 feet_size                    = [-0.07  0.07;   % xMin, xMax
@@ -190,7 +192,7 @@ fZmin                        = 10;
 
 %% Regularization parameters
 Reg.pinvDamp_nu_b = 1e-7;
-Reg.pinvDamp      = 0.01; 
+Reg.pinvDamp      = 0.1; 
 Reg.pinvTol       = 1e-5;
 Reg.impedances    = 0.1;
 Reg.dampings      = 0;
--- torque-controllers/momentum-based-yoga/src/stateMachineYoga.m
+++ torque-controllers/momentum-based-yoga/src/stateMachineYoga.m
@@ -1,4 +1,4 @@
-function  [w_H_b, CoM_des, qj_des, constraints, impedances, KPCoM, KDCoM, currentState, jointsSmoothingTime] = ...
+function  [w_H_b, CoM_des, qj_des, constraints, impedances, KPCoM, KDCoM, KICoM, currentState, jointsSmoothingTime] = ...
             stateMachineYoga(CoM_0, qj_0, l_sole_CoM, r_sole_CoM, qj, t, wrench_rightFoot, wrench_leftFoot, l_sole_H_b, r_sole_H_b, Sm, Gain)
     
     persistent state;
@@ -17,7 +17,8 @@ function  [w_H_b, CoM_des, qj_des, constraints, impedances, KPCoM, KDCoM, curren
     w_H_b        = eye(4);
     impedances   = Gain.impedances(1,:);
     KPCoM        = Gain.KP_COM(1,:);   
-    KDCoM        = Gain.KD_COM(1,:);   
+    KDCoM        = Gain.KD_COM(1,:); 
+    KICoM        = Gain.KI_COM(1,:);
 
     %% TWO FEET BALANCING
     if state == 1 
