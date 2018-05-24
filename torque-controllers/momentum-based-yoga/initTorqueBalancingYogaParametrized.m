f2a0e243ad2ec7b8aa195f639b1c867928c8bb97
Ahmad Gazar <gazar.1692741@studenti.uniroma1.it>
2018-05-24 16:24:07 +0200

    changing initial conditions

--- torque-controllers/momentum-based-yoga/initTorqueBalancingYogaParametrized.m
+++ torque-controllers/momentum-based-yoga/initTorqueBalancingYogaParametrized.m
@@ -28,7 +28,7 @@ clc
 % To do this, you can uncomment the 
 
 % setenv('YARP_ROBOT_NAME','iCubGenova04');
-  setenv('YARP_ROBOT_NAME','icubGazeboSim');
+ setenv('YARP_ROBOT_NAME','icubGazeboSim');
 % setenv('YARP_ROBOT_NAME','iCubGenova02');
 % setenv('YARP_ROBOT_NAME','iCubGazeboV2_5');  
 
